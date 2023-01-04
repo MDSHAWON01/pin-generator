c85c473c2f8ad9204a5c42c401fd730d79b6e358
MD SHAWON <update.shawon5381@gmail.com>
2023-01-04 13:14:46 +0600

    pin-generate

---
+++ pin.js
@@ -0,0 +1,47 @@
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin;
+    }
+    else {
+        return getPin();
+    }
+}
+
+function generatePin() {
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
+}
+
+document.getElementById('key-pad').addEventListener('click', function (e) {
+    const number = e.target.innerText;
+    const calcInput = document.getElementById('typed-number')
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    }
+    else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+})
+
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumber = document.getElementById('typed-number').value;
+    const successMsg = document.getElementById('notify-success')
+    const failError = document.getElementById('notify-failed')
+
+    if (pin == typedNumber) {
+
+        successMsg.style.display = 'block'
+        failError.style.display = 'none'
+    }
+    else {
+        successMsg.style.display = 'none'
+        failError.style.display = 'block'
+    }
+}
